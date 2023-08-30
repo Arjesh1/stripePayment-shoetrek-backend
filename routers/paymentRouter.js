1beed70ab77d367e02058ad653752d2d13bd13bd
Arjesh <arjesh.khadka2@gmail.com>
2023-08-30 15:33:45 +1000

    stripe backend

--- routers/paymentRouter.js
+++ routers/paymentRouter.js
@@ -1,33 +1,31 @@
-import express from "express"
-import Stripe from "stripe"
+import express from "express";
+import Stripe from "stripe";
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/create-payment-intent', async(req, res) =>{
-    const secret = process.env.STRIPE_SK
-    const stripe  = new Stripe(secret)
+router.post("/create-payment-intent", async (req, res) => {
+  const secret = process.env.STRIPE_SK;
+  const stripe = new Stripe(secret);
 
-    const {amount, currency } = req.body
+  const { amount, currency } = req.body;
 
-    try {
-        const paymentIntent = await stripe.paymentIntents.create({
-            amount, 
-            currency,
-            automatic_payment_methods: {enabled: true}
-        })
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount,
+      currency,
+      automatic_payment_methods: { enabled: true },
+    });
 
-        res.status(200).send({
-            clientSecret: paymentIntent.client_secret
-        })
-        
-    } catch (error) {
+    res.status(200).send({
+      clientSecret: paymentIntent.client_secret,
+    });
+    console.log(paymentIntent);
+  } catch (error) {
+    res.status(500).json({
+      status: 500,
+      message: error.message,
+    });
+  }
+});
 
-        res.status(500).json({
-            status: 500,
-            message: error.message,
-        })
-        
-    }
-})
-
-export default router
+export default router;
