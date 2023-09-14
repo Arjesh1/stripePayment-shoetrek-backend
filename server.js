47078b202667271e83f4ac0a272c003421f0c861
Arjesh <arjesh.khadka2@gmail.com>
2023-09-14 10:03:49 +1000

    cors

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
-app.use(cors({ origin: "https://shoe-trek.vercel.app" }));
+app.use(cors());
 
 app.use("/shoe-trek/payment", paymentRouter);
 
